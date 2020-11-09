90f169f905a1969defaf618c7fb18ee37a4550f7
Susmoy <susmoymukherjee@gmail.com>
2020-11-09 21:05:32 +0530

    appointy first commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//type JSONTime time.Time
+
+// func (t JSONTime) MarshalJSON() ([]byte, error) {
+// 	//do your serializing here
+// 	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("Mon Jan _2"))
+// 	return []byte(stamp), nil
+//}
+
+type Article struct {
+	Id       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title    string             `json:"title" bson:"title,omitempty"`
+	SubTitle string             `json:"subtitle" bson:"subtitle,omitempty"`
+	Content  string             `json:"content" bson:"content,omitempty"`
+	Creation time.Time          `json:"creation" bson:"creation,omitempty"`
+}
+
+
+var collection = ConnectDB()
+
+func main() {
+	
+	r := mux.NewRouter()
+
+	
+	r.HandleFunc("/articles", getArticles).Methods("GET")
+	r.HandleFunc("/articles", createArticle).Methods("POST")
+	r.HandleFunc("/articles/{id}", getArticle).Methods("GET")
+	r.HandleFunc("/articles/search?q=title", getArticle).Methods("GET")
+
+	
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
